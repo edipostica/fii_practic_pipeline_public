858de0fb4716806f19c7a5a7570414f6eab60a9b
edipostica <153132198+edipostica@users.noreply.github.com>
2024-04-06 10:42:08 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -17,7 +17,7 @@ module "s3-terraform-state" {
   source  = "cloudposse/s3-bucket/aws"
   version = "3.1.3"
 
-  bucket_name        = "s3-terraform-state-fii-practic-iuewyediouwetf65"
+  bucket_name        = "s3-terraform-state-fii-practic-iuewyediouwet65"
   versioning_enabled = true
 }
 
